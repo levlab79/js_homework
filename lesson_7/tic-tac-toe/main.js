e51fe15decd39c71bc3db2056d2d3feee9ef2a4f
levlab79 <lev.lsb56@gmail.com>
2021-02-01 18:32:28 +0400

    lesson_7

---
+++ lesson_7/tic-tac-toe/main.js
@@ -0,0 +1,97 @@
+const equals3 = (a, b, c) => {
+  return (a == b && b == c && a != '');
+}
+
+const checkWinner = (model) => {
+  let winner = null;
+
+  // horizontal
+  for (let i = 0; i < 3; i++) {
+    if (equals3(model[i][0], model[i][1], model[i][2])) {
+      winner = model[i][0];
+    }
+  }  
+
+  // vertical
+  for (let i = 0; i < 3; i++) {
+    if (equals3(model[0][i], model[1][i], model[2][i])) {
+      winner = model[0][i];
+    }
+  }
+
+  // diagonal
+  if (equals3(model[0][0], model[1][1], model[2][2])) {
+    winner = model[0][0];
+  }
+  if (equals3(model[0][2], model[1][1], model[2][0])) {
+    winner = model[0][2];
+  }
+
+  return winner;
+}
+
+const checkDraw = (model) => {
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (model[i][j] == '') {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+const game = () => {
+  const model = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
+  ];
+  const playerX = 'X';
+  const playerO = 'O';
+  let currentPlayer = playerX;
+
+  const $game = document.querySelector('#game');
+  const $table = document.createElement('table');
+
+  for (let i = 0; i < 3; i++) {
+    const $tr = document.createElement('tr');
+    $tr.dataset.index = i;
+
+    for (let j = 0; j < 3; j++) {
+      const $td = document.createElement('td');
+      $td.dataset.index = j;
+      $tr.appendChild($td);
+    }
+
+    $table.appendChild($tr);
+  }
+
+  $game.appendChild($table);
+
+  $table.addEventListener('click', (e) => {
+    const row = e.target.parentNode.dataset.index;
+    const column = e.target.dataset.index;
+
+    // Проверяем можно ли рисовать в этой клетке.
+    if (model[row][column] == '') {
+      model[row][column] = currentPlayer;
+      e.target.innerHTML = currentPlayer;
+  
+      const winner = checkWinner(model);
+      if (winner) {
+        alert(`Winner: ${winner}`);
+      // Проверка на ничью.
+      } else if (checkDraw(model)) {
+        alert('Draw');
+      }
+    } else {
+      alert('The place is not empty!');
+    }
+    currentPlayer = currentPlayer === playerX ? playerO : playerX;
+  });
+}
+
+window.onload = () => {
+  game();
+};
